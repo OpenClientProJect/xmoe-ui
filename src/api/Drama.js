9a197db1d057f8b0f6e24b4669df7ae6dce8ef89
LiggPlus <wenzhouli06@gmail.com>
2025-04-15 19:07:32 +0800

    更新接口

--- src/api/Drama.js
+++ src/api/Drama.js
@@ -1,11 +1,28 @@
 import request from '@/utils/request.js'
 
-// 获取番剧列表
-export const getDramaListService = () =>{
-    return request.get('/drama/dramaList')
-}
-
-// 获取番剧详情
-export const getDramaDetailService = (id) =>{
-    return request.get(`/drama/dramaInfo?vodId=` + id)
+/**
+ * 获取番剧列表
+ * @param {Object} params 请求参数
+ * @returns {Promise} 请求Promise
+ */
+export const getDramaListService = (params = {}) => {
+  // 设置默认参数
+  const defaultParams = {
+    typeId: 1,
+    page: 1,
+    limit: 18,
+    type: 'updateTime'
+  }
+  
+  // 合并默认参数和传入参数
+  const finalParams = { ...defaultParams, ...params }
+  
+  return request.get('/vod/list', {
+    params: finalParams,
+    headers: {
+      'User-Agent': 'Dart/2.10 (dartio)',
+      'Accept-Encoding': 'gzip',
+      'content-length': '0'
+    }
+  })
 }
